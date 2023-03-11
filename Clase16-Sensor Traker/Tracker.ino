e92dd5854c3c08bb5753632786f4280a0769fda4
Daniela CB <115411941+DanielaJanethCruz@users.noreply.github.com>
2023-03-11 01:38:41 -0600

    Add files via upload

---
+++ Clase16-Sensor Traker/Tracker.ino
@@ -0,0 +1,17 @@
+//Sensor Tracker
+int sensorPin = 2;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  bool value = digitalRead(sensorPin);
+  
+  if(value == 0){
+    Serial.println("Blanco");
+  } else {
+    Serial.println("Negro");
+  }
+}
