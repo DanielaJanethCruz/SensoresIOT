579767a6fd69557ae39c78a1e4b827442066741f
Daniela CB <115411941+DanielaJanethCruz@users.noreply.github.com>
2023-03-11 01:11:19 -0600

    Add files via upload

---
+++ Clase21-Motor Corriente Continua con Sensor Tilt Switch/TiltSwitch-CorrienteContinua.ino
@@ -0,0 +1,17 @@
+//Tilt Switch con Motor Corriente continua
+int pint=2;
+
+#define SENSOR_PIN 14
+
+void setup() {
+  // sets the pins as outputs:
+  pinMode(pint, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Move the DC motor forward at maximum speed
+  digitalWrite(pint, digitalRead(SENSOR_PIN));
+}
